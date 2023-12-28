dfa6537fc79023fbeaa612326f29a67f170b793a
Alexandre Lara <ale_larajr@hotmail.com>
2023-12-28 19:27:59 -0300

    build: Adição do github pages junto junto com builds

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import pencil from "./assets/icons/pencil.svg";
+import lamp from "./assets/icons/lamp.svg";
+import trophy from "./assets/icons/trophy.svg";
+import exclamation from "./assets/icons/exclamation.svg";
+import flag from "./assets/icons/flag.svg";
+import handshake from "./assets/icons/handshake.svg";
+import passionVisibility from "./assets/icons/passionVisibility.svg";
+
 export default function App() {
     return (
         <div className="border-2 h-screen w-screen">
@@ -8,7 +16,9 @@ export default function App() {
                         <a className="text-white">HOME</a>
                       </li> */}
                     <li>
-                        <a className="text-white cursor-pointer">Sobre</a>
+                        <a href="#about" className="text-white cursor-pointer">
+                            Sobre
+                        </a>
                     </li>
                     <li>
                         <a
@@ -30,15 +40,15 @@ export default function App() {
                 </button>
             </header>
             <main>
-                <div className="border-2 w-full h-auto flex flex-col  mt-16">
+                <div className=" w-full h-auto flex flex-col  mt-72 mb-96">
                     <h1 className="text-8xl pl-20 -mt-2">dads</h1>
                     <h3 className="text-xl pl-20">
                         Mais que uma agencia de comunicação, uma agencia de
                         criação!
                     </h3>
                 </div>
-                <div className="border-2 w-full h-auto flex flex-col ">
-                    <div className="border-2 h-auto w-6/12 ml-20 mb-20">
+                <div id="about" className=" w-full h-auto flex flex-col ">
+                    <div className="h-auto w-6/12 ml-20 mb-20">
                         <h3 className="text-4xl">Quem somos</h3>
                         <p className="p-4">
                             Na vastidão digital, somos os arquitetos do sucesso,
@@ -60,25 +70,119 @@ export default function App() {
                             mundo digital, sua história é nossa prioridade.
                         </p>
                     </div>
-                    <div className="border-2 w-5/6 flex flex-col justify-center mx-auto">
-                        <h3 className="text-4xl flex justify-center">
+                    <div className="w-full flex flex-col justify-center mx-auto">
+                        <h3 className="text-4xl flex justify-center mb-12">
                             Valores
                         </h3>
-                        <ul className="flex flex-row">
-                            <li className="">lapis Excelência Criativa</li>
-                            <li> lampada Compromisso Inovador</li>
-                            <li>trofeu Orientação para o sucesso</li>
-                            <li> exclamacao Atenção Personalizada</li>
-                            <li>bandeira Prioridade na Narrativa</li>
-                            <li>handshake Parceria Transparente</li>
-                            <li>
-                                mao segurando coracao Paixão pela Visibilidade
-                            </li>
-                        </ul>
+                        <div className="flex flex-row justify-evenly  ">
+                            <div className="h-40 w-40 flex flex-col rounded-xl ">
+                                <div className="h-24 w-24 grid self-center bg-yellow-100 rounded-full">
+                                    <img
+                                        className="h-16  m-auto"
+                                        src={pencil}
+                                        alt=""
+                                    />
+                                </div>
+                                <h4 className="text-center my-auto">
+                                    Excelência Criativa
+                                </h4>
+                            </div>
+                            <div className="h-40 w-40 flex flex-col rounded-xl ">
+                                <div className="h-24 w-24 grid self-center bg-yellow-100 rounded-full">
+                                    <img
+                                        className="h-16  m-auto"
+                                        src={lamp}
+                                        alt=""
+                                    />
+                                </div>
+                                <h4 className="text-center my-auto">
+                                    Compromisso Inovador
+                                </h4>
+                            </div>
+                            <div className="h-40 w-40 flex flex-col rounded-xl ">
+                                <div className="h-24 w-24 grid self-center bg-yellow-100 rounded-full">
+                                    <img
+                                        className="h-16  m-auto"
+                                        src={trophy}
+                                        alt=""
+                                    />
+                                </div>
+                                <h4 className="text-center my-auto">
+                                    Orientação para o sucesso
+                                </h4>
+                            </div>
+                            <div className="h-40 w-40 flex flex-col rounded-xl ">
+                                <div className="h-24 w-24 grid self-center bg-yellow-100 rounded-full">
+                                    <img
+                                        className="h-16  m-auto"
+                                        src={exclamation}
+                                        alt=""
+                                    />
+                                </div>
+                                <h4 className="text-center my-auto">
+                                    Atenção Personalizada
+                                </h4>
+                            </div>
+                            <div className="h-40 w-40 flex flex-col rounded-xl ">
+                                <div className="h-24 w-24 grid self-center bg-yellow-100 rounded-full">
+                                    <img
+                                        className="h-16  m-auto"
+                                        src={flag}
+                                        alt=""
+                                    />
+                                </div>
+                                <h4 className="text-center my-auto">
+                                    Prioridade na Narrativa
+                                </h4>
+                            </div>
+
+                            <div className="h-40 w-40 flex flex-col rounded-xl ">
+                                <div className="h-24 w-24 grid self-center bg-yellow-100 rounded-full">
+                                    <img
+                                        className="h-16  m-auto"
+                                        src={handshake}
+                                        alt=""
+                                    />
+                                </div>
+                                <h4 className="text-center my-auto">
+                                    Parceria Transparente
+                                </h4>
+                            </div>
+                            <div className="h-40 w-40 flex flex-col rounded-xl ">
+                                <div className="h-24 w-24 grid self-center bg-yellow-100 rounded-full">
+                                    <img
+                                        className="h-16  m-auto"
+                                        src={passionVisibility}
+                                        alt=""
+                                    />
+                                </div>
+                                <h4 className="text-center my-auto">
+                                    Paixão pela Visibilidade
+                                </h4>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="border-2 w-full h-auto" id="services">
-                    <h2>Servicos</h2>
+                    <h2>Serviços</h2>
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                            <p></p>
+                        </div>
+                        <div>
+                            <img src="" alt="" />
+                            <p></p>
+                        </div>
+                        <div>
+                            <img src="" alt="" />
+                            <p></p>
+                        </div>
+                        <div>
+                            <img src="" alt="" />
+                            <p></p>
+                        </div>
+                    </div>
                 </div>
 
                 <div>Portfólio</div>
