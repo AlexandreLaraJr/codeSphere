2c96a54f44a004bc45fbedc9e8f60b6397e6d77b
Alexandre Lara <ale_larajr@hotmail.com>
2024-01-18 19:20:48 -0300

    feat: Alteração na home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
             id="home"
             className="w-full h-full flex flex-col -mt-96 md:-mt-96 py-52 -mb-12 md:mb-40 scroll-mt-96"
         >
-            <h1 className="font-bold text-lime-500 ml-8 mb-4 md:ml-20">
+            <h1 className="font-bold text-lime-500 ml-8 mb-4 -mt-2 md:mt-24 md:ml-20 ">
                 CodeSphere
             </h1>
             <h3 className="text-2xl ml-8 md:ml-20">
